c1aae7be27536cab6d0c846dedd01e3a0d4b4844
Léo Melo 147 <lahmjj@gmail.com>
2025-11-06 01:44:38 -0300

    feat: create QuantityDTO

--- src/main/java/one/digitalinnovation/beerstock/dto/QuantityDTO.java
+++ src/main/java/one/digitalinnovation/beerstock/dto/QuantityDTO.java
@@ -1,20 +1,11 @@
 package one.digitalinnovation.beerstock.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class QuantityDTO {
-
-    @NotNull
-    @Max(100)
-    private Integer quantity;
+    @NotNull @Max(100) private Integer quantity;
 }
