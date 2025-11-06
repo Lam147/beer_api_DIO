76964f866d4338a57656f9bbc2fa6599aac928fe
Léo Melo 147 <lahmjj@gmail.com>
2025-11-06 01:44:17 -0300

    feat: create BeerDTO

--- src/main/java/one/digitalinnovation/beerstock/dto/BeerDTO.java
+++ src/main/java/one/digitalinnovation/beerstock/dto/BeerDTO.java
@@ -1,42 +1,18 @@
 package one.digitalinnovation.beerstock.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import one.digitalinnovation.beerstock.enums.BeerType;
-
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import lombok.*;
+import one.digitalinnovation.beerstock.enums.BeerType; 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class BeerDTO {
-
     private Long id;
-
-    @NotNull
-    @Size(min = 1, max = 200)
-    private String name;
-
-    @NotNull
-    @Size(min = 1, max = 200)
-    private String brand;
-
-    @NotNull
-    @Max(500)
-    private Integer max;
-
-    @NotNull
-    @Max(100)
-    private Integer quantity;
-
-    @Enumerated(EnumType.STRING)
-    @NotNull
-    private BeerType type;
+    @NotNull @Size(min = 1, max = 200) private String name;
+    @NotNull @Size(min = 1, max = 200) private String brand;
+    @NotNull @Max(500) private Integer max;
+    @NotNull @Max(100) private Integer quantity;
+    @NotNull private BeerType type;
 }
